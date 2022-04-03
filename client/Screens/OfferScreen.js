907a12f70558671bf38e42782f0ad32114438731
Enes koç <32553624+eneskocc@users.noreply.github.com>
2022-04-03 03:55:49 +0300

    update

--- client/Screens/OfferScreen.js
+++ client/Screens/OfferScreen.js
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Button, Image, View, Platform } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
-const OfferScreen = () => {
+export default function OfferScreen() {
+  const [image, setImage] = useState(null);
+  const img = [];
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+    img.push(image);
+    console.log(img);
+  };
+
   return (
-    <View>
-      <Text>OfferScreen</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && 
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+       }  
     </View>
-  )
+  );
 }
-
-export default OfferScreen
-
-const styles = StyleSheet.create({})
